de89b877d46b0273fc939d43cf3fbdd0cdb10f2e
kalidouBA <kalidou.ia.mlds@gmail.com>
2022-01-14 11:29:01 +0100

    Change version

--- CytOpT/cytopt_version.py
+++ CytOpT/cytopt_version.py
@@ -3,8 +3,8 @@ __version__.py
 Information about the current version of the package.
 """
 
-__title__ = 'CytOpt'
-__version__ = '1.0.1'
+__title__ = 'CytOpT'
+__version__ = '2022.0'
 __author__ = ['Paul Freulon', 'Kalidou BA', 'Boris Hejblum']
 __author_email__ = 'paul.freulon@math.u-bordeaux.fr'
 __license__ = 'MIT'
